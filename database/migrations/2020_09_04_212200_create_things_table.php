4b164596cd67c30a10ffdc7b4ceb70fee05bf2d5
Joaquim Perez <jivkim@gmail.com>
2020-09-04 23:24:47 +0200

    model+migration Thing

---
+++ database/migrations/2020_09_04_212200_create_things_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('things', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('parent_id')->constrained('things');
+            $table->string('name');
+            $table->string('key');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('things');
+    }
+}
