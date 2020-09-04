7e2ec0759d0e0a0e6f98a8bdca51c1ffb5b01cf6
Joaquim Perez <jivkim@gmail.com>
2020-09-04 23:51:52 +0200

    create a Place in seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Place;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $place = new Place();
+        $place->key = "Neuve 3";
+        $place->save();
     }
 }
