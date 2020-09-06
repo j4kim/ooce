aa4abe59619b15e746010a71692a9a1b634da4b8
Joaquim Perez <jivkim@gmail.com>
2020-09-06 21:39:39 +0200

    add parent relationship + don't eager load children (we can't eager load both due to infinite loop)

--- app/Thing.php
+++ app/Thing.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thing extends Model
 {
-    protected $with = ['children'];
-
     protected $guarded = [];
 
     public function children()
     {
         return $this->hasMany('App\Thing', 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('App\Thing', 'parent_id');
     }
 }
