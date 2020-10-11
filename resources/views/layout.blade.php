a5bd2a906623ff249be03f46e1d506cd3b0845f7
Joaquim Perez <jivkim@gmail.com>
2020-10-11 11:59:59 +0200

    Add bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,70 +2,26 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title>Oocé</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            h2 {
-                font-weight: lighter;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            a {
-                text-decoration: none;
-            }
-        </style>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <h1 class="title m-b-md">
-                    Oocé
-                </h1>
+        <header class="text-white bg-primary">
+            <div class="p-2 container d-flex justify-content-between align-items-center">
+                <h1 class="m-0">Oocé</h1>
                 <form action="search" method="get" class="m-b-md">
-                    <input type="search" name="q" placeholder="Rechercher un truc">
+                    <input class="form-control" type="search" name="q" placeholder="Rechercher un truc">
                 </form>
-                @yield('content')
             </div>
-        </div>
+        </header>
+        <main class="container pt-4">
+            @yield('content')
+        </main>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     </body>
 </html>
