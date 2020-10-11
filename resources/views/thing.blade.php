38d0f95bce0faf776a5181b501636b7982eb5a51
Joaquim Perez <jivkim@gmail.com>
2020-10-11 14:13:24 +0200

    bootstrapize thing view

--- resources/views/thing.blade.php
+++ resources/views/thing.blade.php
@@ -7,8 +7,14 @@
   <p>
     <form action="{{$thing->id}}/update" method="post">
       @csrf @method('PUT')
-      <input type="text" name="name" value="{{$thing->name}}">
-      <button>Renommer</button>
+      <div class="row">
+        <div class="col-12 col-sm-6 col-md-4 mb-2 mb-sm-0">
+          <input class="form-control" type="text" name="name" value="{{$thing->name}}">
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+          <button type="submit" class="btn btn-primary">Renommer</button>
+        </div>
+      </div>
     </form>
   </p>
   <p>
@@ -16,17 +22,28 @@
       <form action="{{ $thing->id }}/detach" method="post">
         @csrf @method('PUT')
         Rangé dans: {{ $thing->parent->name }}
-        <button>Sortir</button>
+        <button type="submit" class="btn btn-primary">
+          Sortir
+        </button>
       </form>
     @else
       <form action="{{ $thing->id }}/move" method="post">
         @csrf @method('PUT')
-        Ranger dans: <input type="number" name="parent_id">
+        Ranger dans:
+        <input
+          style="max-width:100px"
+          class="form-control d-inline"
+          type="number"
+          name="parent_id"
+          min="1"
+        >
       </form>
     @endif
   </p>
   <form action="create" method="post">
     @csrf
-    <button>Ajouter un truc</button>
+    <button type="submit" class="btn btn-primary">
+      Ajouter un truc
+    </button>
   </form>
 @endsection
