e7cb557efd38451c86716322f6ff263ef9cb9e3a
Joaquim Perez <jivkim@gmail.com>
2020-10-11 19:19:32 +0200

    minimalist seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $place = Thing::create(['name' => 'Neuve 3']);
-        $cellar = $place->children()->create(['name' => 'Cave']);
-        $box = $cellar->children()->create(['name' => 'Boîte']);
+        Thing::create(['name' => 'Neuve 3']);
     }
 }
