0d9041174fafe81330377452bfea669b32263cc8
Joaquim Perez <jivkim@gmail.com>
2021-02-07 21:48:10 +0100

    add names to routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
         $thing = Thing::create();
     }
     return redirect($thing->id);
-});
+})->name('index');
 
 Route::get('/search', function (Request $request) {
     $thing = Thing::find($request->q);
@@ -29,32 +29,32 @@ Route::get('/search', function (Request $request) {
         return redirect($thing->id);
     }
     return "No results, sorry";
-});
+})->name('search');
 
 Route::get('/{thing}', function (Thing $thing) {
     return view('thing', compact('thing'));
-});
+})->name('show');
 
 Route::get('/{thing}/edit', function (Thing $thing) {
     return view('edit', compact('thing'));
-});
+})->name('edit');
 
 Route::post('/{thing}/add', function (Thing $thing, Request $request) {
     $newthing = $thing->children()->create($request->all());
     return redirect($newthing->id);
-});
+})->name('add');
 
 Route::put('/{thing}/update', function (Thing $thing, Request $request) {
     $thing->update($request->all());
     return redirect($thing->id);
-});
+})->name('update');
 
 Route::put('/{thing}/move', function (Thing $thing, Request $request) {
     $thing->moveTo($request->parent_id);
     return redirect($thing->id);
-});
+})->name('move');
 
 Route::put('/{thing}/detach', function (Thing $thing) {
     $thing->moveTo(null);
     return redirect($thing->id);
-});
+})->name('detach');
