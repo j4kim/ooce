a58b2849c71d178ab916d70c501408ab4599a17b
Joaquim Perez <jivkim@gmail.com>
2021-02-07 22:33:42 +0100

    Modify button

--- resources/views/thing.blade.php
+++ resources/views/thing.blade.php
@@ -4,6 +4,7 @@
   <h2>
     {{$thing->id}} <b>{{$thing->name}}</b>
   </h2>
+  <a href="{{ route('edit', $thing->id) }}" class="btn btn-light">Modifier</a>
   <form class="mt-4" action="{{ route('add', $thing->id) }}" method="post">
     @csrf
     <button type="submit" class="btn btn-primary">
