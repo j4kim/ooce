afe64fce52b4c0e38a87aeefcc477328da4dbb27
Joaquim Perez <jivkim@gmail.com>
2021-02-07 22:58:09 +0100

    remove move and detach form in edition view

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -8,38 +8,22 @@
     <form action="{{ route('update', $thing->id) }}" method="post">
       @csrf @method('PUT')
       <div class="row">
-        <div class="col-12 col-sm-6 col-md-4 mb-2 mb-sm-0">
+        <div class="col-12 col-sm-6 col-md-4 mb-2">
+          Nom:
           <input class="form-control" type="text" name="name" autofocus value="{{$thing->name}}">
         </div>
-        <div class="col-12 col-sm-6 col-md-4">
+        <div class="col-12 mb-2">
+          Description:
+          <textarea class="form-control" name="description" class="btn btn-primary">{{ $thing->description}}</textarea>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4 mb-2">
+          Rangé dans:
+          <input class="form-control" name="parent_id" value="{{ $thing->parent_id }}">
+        </div>
+        <div class="col-12 mt-2">
           <button type="submit" class="btn btn-primary">Enregistrer</button>
         </div>
       </div>
     </form>
-  </p>
-  <p>
-    @if ($thing->parent)
-      <form action="{{ route('detach', $thing->id) }}" method="post">
-        @csrf @method('PUT')
-        Rangé dans:
-        <a href="{{ route('show', $thing->parent->id) }}">
-          {{ $thing->parent->name }}
-        </a>
-        <button type="submit" class="btn btn-secondary btn-sm ml-2">
-          Sortir
-        </button>
-      </form>
-    @else
-      <form action="{{ route('move', $thing->id) }}" method="post">
-        @csrf @method('PUT')
-        Ranger dans:
-        <input
-          style="max-width:100px"
-          class="form-control d-inline"
-          name="parent_id"
-          min="1"
-        >
-      </form>
-    @endif
   </p>
 @endsection
