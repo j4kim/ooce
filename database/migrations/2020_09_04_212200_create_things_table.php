83896956a6c75d4c2f97b71474c32d5523764fb8
Joaquim Perez <jivkim@gmail.com>
2021-02-08 23:36:12 +0100

    move thing_container column creation in its own migration

--- database/migrations/2020_09_04_212200_create_things_table.php
+++ database/migrations/2020_09_04_212200_create_things_table.php
@@ -21,7 +21,6 @@ class CreateThingsTable extends Migration
             $table->text('description')->nullable();
             $table->text('picture_path')->nullable();
             $table->timestamp('moved_at')->useCurrent();
-            $table->boolean('thing_container')->default(true);
         });
     }
 
