35f2e8072bb7625df23058f12bcd24cc85aecaac
Joaquim Perez <jivkim@gmail.com>
2021-02-09 22:15:34 +0100

    autofocus doesn't work (¯\_(ツ)_/¯) + mark name input as required

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -10,7 +10,7 @@
       <div class="row">
         <div class="col-12 col-sm-6 col-md-4 mb-2">
           Nom:
-          <input class="form-control" type="text" name="name" autofocus value="{{$thing->name}}">
+          <input class="form-control" type="text" name="name" value="{{$thing->name}}" required>
         </div>
         <div class="col-12 mb-2">
           Description:
