2fd1039e42c8ac13eb805892e86c5a6015e89168
Joaquim Perez <jivkim@gmail.com>
2021-02-09 22:56:28 +0100

    search result page layout

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -2,15 +2,25 @@
 
 @section('content')
   <p>Résultats de la recherche "{{ $query }}"</p>
-  <ul>
-    @forelse ($things as $thing)
-    <li>
-      <a href="{{ route('show', $thing) }}">
-        {{ $thing->id }} <b>{{ $thing->name }}</b>
-      </a>
-    </li>
-    @empty
-    <li>Aucun résultat</li>
-    @endforelse
-  </ul>
+  @forelse ($things as $thing)
+    <div class="card mb-3">
+      <div class="card-body d-flex justify-content-between">
+        <div>
+          <a href="{{ route('show', $thing) }}" class="stretched-link text-decoration-none">
+            <h4>
+              {{ $thing->id }} <b>{{ $thing->name }}</b>
+            </h4>
+          </a>
+          <div>{{ $thing->description }}</div>
+        </div>
+        @if ($thing->picture_path)
+          <div>
+            <img src="{{  Storage::url($thing->picture_path) }}" style="max-height:60px">
+          </div>
+        @endif
+      </div>
+    </div>
+  @empty
+    <p class="text-muted">Aucun résultat...</p>
+  @endforelse
 @endsection
