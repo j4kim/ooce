4e7724369f9c6c3efb6aaeeace2be972932acc00
Joaquim Perez <jivkim@gmail.com>
2021-02-20 21:06:58 +0100

    add a manifest

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
         <title>Oocé</title>
         
         <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
