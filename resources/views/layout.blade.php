d0d7b65ed6d8b004ac79203f5d9b0a7a67a7eeda
Joaquim Perez <jivkim@gmail.com>
2021-03-14 15:51:14 +0100

    add meta tags and links for ios PWA

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,12 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1, maximum-scale=1,  user-scalable=0">
         
         <title>Oocé</title>
         
         <link rel="icon" type="image/png" href="{{ asset('icon-512.png') }}">
         <link rel="manifest" href="{{ asset('manifest.json') }}">
+        <link rel="apple-touch-icon" href="{{ asset('icon-192.png') }}">
+        <link rel="apple-touch-startup-image" href="{{ asset('icon-512.png') }}">
+    
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
