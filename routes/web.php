3cdf8ff9b3c779b1cb2069d5eda9ba010c82743c
Joaquim Perez <jivkim@gmail.com>
2021-07-30 00:59:22 +0200

    Move all things web routes in middleware group auth

--- routes/web.php
+++ routes/web.php
@@ -21,66 +21,71 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', function () {
-    $root = Thing::doesntHave('parent')->orderBy('created_at')->first();
-    $orphans = Thing::doesntHave('parent')
-                    ->where('id', '!=', $root->id)
-                    ->orderBy('moved_at', 'desc')
-                    ->take(20)
-                    ->get();
-    return view('index', compact('root', 'orphans'));
-})->name('index');
 
-Route::post('/create', function () {
-    $thing = Thing::create();
-    return redirect(route('edit', $thing->id));
-})->name('create');
+Route::group(['middleware' => ['auth']], function () {
 
-Route::get('/search/{query}', function ($query) {
-    $things = Thing::search($query);
-    if (count($things) === 1) {
-        return redirect(route('show', $things->first->id));
-    }
-    return view('search-result', compact('things', 'query'));
-})->name('search');
+    Route::get('/', function () {
+        $root = Thing::doesntHave('parent')->orderBy('created_at')->first();
+        $orphans = Thing::doesntHave('parent')
+                        ->where('id', '!=', $root->id)
+                        ->orderBy('moved_at', 'desc')
+                        ->take(20)
+                        ->get();
+        return view('index', compact('root', 'orphans'));
+    })->name('index');
 
-Route::get('/{thing}', function (Thing $thing) {
-    return view('thing', compact('thing'));
-})->name('show');
+    Route::post('/create', function () {
+        $thing = Thing::create();
+        return redirect(route('edit', $thing->id));
+    })->name('create');
 
-Route::get('/{thing}/edit', function (Thing $thing) {
-    return view('edit', compact('thing'));
-})->name('edit');
+    Route::get('/search/{query}', function ($query) {
+        $things = Thing::search($query);
+        if (count($things) === 1) {
+            return redirect(route('show', $things->first->id));
+        }
+        return view('search-result', compact('things', 'query'));
+    })->name('search');
 
-Route::put('/{thing}/duplicate', function (Thing $thing) {
-    $newThing = $thing->replicate();
-    $newThing->save();
-    return redirect(route('show', $newThing));
-})->name('duplicate');
+    Route::get('/{thing}', function (Thing $thing) {
+        return view('thing', compact('thing'));
+    })->name('show');
 
-Route::post('/{thing}/add', function (Thing $thing, Request $request) {
-    $newthing = $thing->children()->create($request->all());
-    return redirect(route('edit', $newthing->id));
-})->name('add');
+    Route::get('/{thing}/edit', function (Thing $thing) {
+        return view('edit', compact('thing'));
+    })->name('edit');
 
-Route::put('/{thing}/update', function (Thing $thing, Request $request) {
-    $attributes = $request->except('picture');
-    if ($request->hasFile('picture')) {
-        $picture_path = $request->file('picture')->store('uploads');
-        $asset_path = Asset::create($picture_path);
-        $attributes = array_merge($attributes, compact('picture_path', 'asset_path'));
-    }
-    $thing->fill($attributes);
-    $thing->thing_container = $request->thing_container === 'on';
-    if ($thing->isDirty('parent_id')) {
-        $thing->moved_at = now();
-    }
-    $thing->save();
-    return redirect(route('show', $thing->id));
-})->name('update');
+    Route::put('/{thing}/duplicate', function (Thing $thing) {
+        $newThing = $thing->replicate();
+        $newThing->save();
+        return redirect(route('show', $newThing));
+    })->name('duplicate');
 
-Route::delete('/{thing}', function (Thing $thing) {
-    $parentId = $thing->parent_id;
-    $thing->delete();
-    return redirect($parentId ? route('show', $parentId) : '/');
-})->name('delete');
+    Route::post('/{thing}/add', function (Thing $thing, Request $request) {
+        $newthing = $thing->children()->create($request->all());
+        return redirect(route('edit', $newthing->id));
+    })->name('add');
+
+    Route::put('/{thing}/update', function (Thing $thing, Request $request) {
+        $attributes = $request->except('picture');
+        if ($request->hasFile('picture')) {
+            $picture_path = $request->file('picture')->store('uploads');
+            $asset_path = Asset::create($picture_path);
+            $attributes = array_merge($attributes, compact('picture_path', 'asset_path'));
+        }
+        $thing->fill($attributes);
+        $thing->thing_container = $request->thing_container === 'on';
+        if ($thing->isDirty('parent_id')) {
+            $thing->moved_at = now();
+        }
+        $thing->save();
+        return redirect(route('show', $thing->id));
+    })->name('update');
+
+    Route::delete('/{thing}', function (Thing $thing) {
+        $parentId = $thing->parent_id;
+        $thing->delete();
+        return redirect($parentId ? route('show', $parentId) : '/');
+    })->name('delete');
+
+});
